eeb552bcfac1bcc0f2ee5da4555f05b1a049b3f5
Gabriel Araújo <10gabrielaraujo@gmail.com>
2024-08-01 14:42:02 -0300

    .

--- Meus projetos/SuperHeroAPI/SuperHeroAPI/Data/DataContext.cs
+++ Meus projetos/SuperHeroAPI/SuperHeroAPI/Data/DataContext.cs
@@ -6,5 +6,5 @@ public class DataContext : DbContext
 {
     public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
-    public DbSet<SuperHero> SuperHeroes { get; set.; }
+    public DbSet<SuperHero> SuperHeroes { get; set; }
 }
